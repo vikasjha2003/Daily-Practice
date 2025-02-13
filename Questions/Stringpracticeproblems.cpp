7746fd7b1e100677c11597a0cc5074f50178a945
vikasjha2003 <vikas2003jha@gmail.com>
2025-02-13 12:28:33 +0530

    Strings 1

---
+++ Questions/Stringpracticeproblems.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<sstream>
+using namespace std;
+int main () {
+
+
+// Input a string of size n and update all the odd positions in the string to character ‘#’.
+// string str;
+// cout<<"Enter the string: ";
+// getline(cin,str);
+// for(int i = 0; i<str.length(); i++) {
+//     if(i%2==0) str[i] = '#';
+// }
+// cout<<str<<" "<<endl;
+
+
+// //Input a string of length n and count all the consonants in the given string.
+// cout<<"Enter the string: ";
+// getline(cin,str);
+// int count = 0;
+// for(int i = 0; i<str.length(); i++) {
+//     if(str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u') count++;
+// }
+// cout<<count<<endl;
+
+
+// Check whether the given string is palindrome or not.
+    // string str;
+    // cout<<"Enter the string: ";
+    // getline(cin,str);
+    // int j = str.length()-1;
+    // bool flag = true;
+    // for(int i = 0; i<=j; i++) {
+    //     if(str[i]!=str[j]) {
+    //         flag = false;
+    //         break;
+    //     } else j--;
+    // }
+    // if(flag==true) {
+    //     cout<<"Palindrome"<<endl;
+    // } else {
+    //     cout<<"Not Palindrome"<<endl;
+    // }
+
+
+    // // Input a string of even length and reverse the second half of the string.
+    // reverse(str.begin()+str.length()/2,str.end());
+    // cout<<str<<endl;
+
+
+    // // Input a string of length less than 10 and convert it into integer without using builtin function.
+
+}
