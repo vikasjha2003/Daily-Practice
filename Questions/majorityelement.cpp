be898c66153a2ed31a8d2f2bed03026466f9bb32
vikasjha2003 <vikas2003jha@gmail.com>
2025-05-01 22:52:49 +0530

    Optimised majority element

--- Questions/majorityelement.cpp
+++ Questions/majorityelement.cpp
@@ -3,38 +3,91 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-int majority (vector<int> nums) {
-    sort(nums.begin(), nums.end());
-        int major = nums[0];
-        if(nums.size()==1) return major;
-        int amt = 0;
-        int c = 0;
-        int j = 0;
-        for(int i = 0; i<nums.size(); i++) {
-            while(j<nums.size()) {
-                if(j==nums.size()-1) {
-                    c++;
-                    break;
-                }
-                if(nums[j]==nums[j+1]) {
-                    c++;
-                    j++;
-                } else {
-                    break;
-                }
+
+//most trash approach, even worse then brute force
+
+// int majority (vector<int> nums) {
+//     sort(nums.begin(), nums.end());
+//         int major = nums[0];
+//         if(nums.size()==1) return major;
+//         int amt = 0;
+//         int c = 0;
+//         int j = 0;
+//         for(int i = 0; i<nums.size(); i++) {
+//             while(j<nums.size()) {
+//                 if(j==nums.size()-1) {
+//                     c++;
+//                     break;
+//                 }
+//                 if(nums[j]==nums[j+1]) {
+//                     c++;
+//                     j++;
+//                 } else {
+//                     break;
+//                 }
+//             }
+//             if(c>amt) {
+//                 amt = c;
+//                 major = nums[j];
+//                 c = 0;
+//                 j++;
+//             } else {
+//                 c = 0;
+//                 j++;
+//             }
+//         }
+//         return major;
+// }
+
+// BRUTE FORCE
+
+// int majority (vector<int> nums) {
+//     for(int val : nums) {
+//         int count = 0;
+//         for(int i : nums) {
+//             if(i==val) count++;
+//         }
+//         if(count > nums.size()/2) {
+//             return val;
+//         }
+//     }
+// }
+
+// BETTER
+// int majority (vector<int> nums) {
+//     sort(nums.begin(), nums.end());
+//     int count = 1;
+//     int ans = nums[0];
+//     for(int i = 1; i < nums.size(); i++) {
+//         if(nums[i]==nums[i-1]) {
+//             count++;
+//         } else{
+//             count = 1;
+//             ans = nums[i];
+//         }
+//         if(count > nums.size()/2) {
+//             return ans;
+//         }
+//     }
+//     return ans;
+// }
+
+// BEST (MOORE'S ALGO)
+    int majority (vector<int> nums) {
+        int count = 0;
+        int ans = nums[0];
+        for(int i = 0; i < nums.size(); i++) {
+            if (count==0) {
+                ans = nums[i];
             }
-            if(c>amt) {
-                amt = c;
-                major = nums[j];
-                c = 0;
-                j++;
+            if(nums[i]==ans) {
+                count++;
             } else {
-                c = 0;
-                j++;
+                count--;
             }
         }
-        return major;
-}
+        return ans;
+    }
 int main () {
     int n;
     cout<<"Enter the size of array: ";
