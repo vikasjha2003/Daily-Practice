d769c74d72a14c438982ba85dde6f3250e3cc531
vikasjha2003 <vikas2003jha@gmail.com>
2025-05-25 16:29:34 +0530

    cycle

--- C++/Sorting/cyclesort.cpp
+++ C++/Sorting/cyclesort.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+// Only apply if array given 1-n or 0-n
+void cycle(vector<int> &nums) {
+    int i = 0;
+    while(i<nums.size()) {
+        int idx = nums[i] -1;
+        if(i == idx) i++;
+        else swap(nums[i],nums[idx]); // placing each element at their correct index
+    }
+}
 int main () {
-    
+    vector<int> v = {5,4,3,2,1};
+    for(int val : v) {
+        cout<<val<<" ";
+    }cout<<endl;
+    cycle(v);
+    for(int val : v) {
+        cout<<val<<" ";
+    }cout<<endl;
 }
