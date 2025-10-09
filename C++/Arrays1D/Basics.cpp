366e5952adc6d0c5047733d530b1b5b71014d1b7
vikasjha2003 <vikas2003jha@gmail.com>
2025-10-09 21:58:58 +0530

    few changes in basic

--- C++/Arrays1D/Basics.cpp
+++ C++/Arrays1D/Basics.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 int main () {
-    int x [6];
-    for(int i = 0; i<6; i++) {
+    int n;
+    cin>>n;
+    int x [n];
+    for(int i = 0; i<n; i++) {
         cout<<"Enter x["<<i<<"] = ";
         cin>>x[i];
     }
-    for(int i = 0; i<6; i++) {
+    for(int i = 0; i<n; i++) {
         cout<<x[i]<<"\t";
     }
 }
