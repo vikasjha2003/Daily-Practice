0c879ad748ccfe926ac4198476a59119c29f24a2
vikasjha2003 <vikas2003jha@gmail.com>
2025-11-11 23:29:58 +0530

    Binary tree traversals practiced

---
+++ C++/Trees/traversal.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<queue>
+#include<queue>
+using namespace std;
+class node {
+    public:
+    int val;
+    node* left;
+    node* right;
+    node(int val) {
+        this->val = val;
+        left = right = NULL; 
+    }
+};
+
+// RECURSIVE
+
+// DFS
+void preorder(node* root, vector<int> &ans) {
+    if(!root) return;
+    ans.push_back(root->val);
+    preorder(root->left,ans);
+    preorder(root->right,ans);
+}
+void inorder(node* root, vector<int> &ans) {
+    if(!root) return;
+    inorder(root->left,ans);
+    ans.push_back(root->val);
+    inorder(root->right,ans);
+}
+void postorder(node* root, vector<int> &ans) {
+    if(!root) return;
+    postorder(root->left,ans);
+    postorder(root->right,ans);
+    ans.push_back(root->val);
+}
+// BFS
+int size(node* root) {
+    if(!root) return 0;
+    return max(size(root->left),size(root->right)) + 1;
+}
+void level (node* root, int cur, int n, vector<int> &ans) {
+    if(cur == n) {
+        ans.push_back(root->val);
+        return;
+    }
+    level(root->left,cur+1,n,ans);
+    level(root->right,cur+1,n,ans);
+}
+void levelorder(node* root, vector<int> &ans) {
+    int n = size(root);
+    for(int i = 1; i<=n; i++) {
+        level(root,1,i,ans);
+    }
+}
+
+// ITERATIVE
+
+void levelorderit(node* root, vector<int> &ans) {
+    queue<node*> qt;
+    if(root) qt.push(root);
+    while(!qt.empty()) {
+        node* temp = qt.front();
+        qt.pop();
+        ans.push_back(temp->val);
+        if(temp->left) qt.push(temp->left);
+        if(temp->right) qt.push(temp->right);
+    }
+}
+void preorderit(node* root, vector<int> &ans) {
+    
+}
+int main () {
+    // tree creation
+    node * a = new node(1);
+    node * b = new node(2);
+    node * c = new node(3);
+    node * d = new node(4);
+    node * e = new node(5);
+    node * f = new node(6);
+    node * g = new node(7);
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    // store traversals
+    vector<int> display;
+    // preorder(a,display);
+    levelorderit(a,display);
+    for(int i : display) {
+        cout<<i<<" ";
+    } cout<<endl;
+}
