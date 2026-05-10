ea450d8c5b3298fa87170c2bcced47eb4e3716a9
vikasjha2003 <vikas2003jha@gmail.com>
2026-05-10 18:00:40 +0530

    Learned basics of oops in java along with final wrapper class , final keyword, garbage collection.

---
+++ java/oops/Wrapper_class.java
@@ -0,0 +1,24 @@
+
+public class Wrapper_class {
+    public static void main (String[] args) {
+        // Integer a = new Integer(45);
+        Integer a = 10; // better way
+        int b = 10;
+
+        // Wrapper class are those class which wrap a primitive data type in order to add more functionalities to it that is add more methods to it.
+        // Integer is a wrapper class of int
+
+        // final keyword is used to make stuff immutable i.e. prevent others from changing its values
+        // only usable for primitive data type
+
+        final int c = 50;
+        // c = c + 5;  won't change value since integer is declared as final (like const in cpp)
+
+        final int[] arr = {1,2,3};
+
+        arr[0] = 60; // since the object of arr is final this means that the reference variable is final not the values in arr so the values in the array can be changed but the array can't be changed.
+
+        // arr = {6,7,9}; 
+        
+    }
+}
---
+++ java/oops/intro.java
@@ -0,0 +1,57 @@
+public class intro {
+
+    public static class Student {
+
+        // class is like the blueprint
+
+        String name;
+        int mark;
+
+
+        // constructor is a way to add values to the above properties.
+        // this is a keyword to access every object
+
+        Student(int val, String name) {
+            
+            // this -> this keyword acts as a reference to the object which means leats say the object is Student st then this.name is equivalent to writing st.name. We can't so write name of object everytime so this keyword is used and is replaced by the name of object.
+
+            this.name = name;
+            this.mark = val;
+        }
+
+        // this is how you call a constructor from inside another constructor
+
+        Student() {
+            this (0,"No data found");
+            // in this case this points to the Class itself which means Student();
+        }
+
+        // we can't manually call garbage collector in java but we can tell it to inform us when its active.
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println(this + "Object is destroyed");
+        }
+    }
+    public static void main (String[] args) {
+
+        int[] marks = new int[5]; // stores marks of students
+        String[] names = new String[5]; // stores names of students
+
+        // now what if we wish store these things together? normal data structures don't allow that so the need for class arises
+
+        Student st1 = new Student(); // objecct is like an actual entity made using that blueprint
+
+        // "." operator links object to its reference. for eg-> st1.marks or st1.name
+        // new dynamically allocates memory and returns a reference to it
+
+        System.out.println(st1.name);
+        System.out.println(st1.mark);
+
+
+        // this loop shows when garbage collector is called.
+        for(int i = 0; i<1000000; i++) {
+            st1 = new Student();
+        }
+
+    }
+}
